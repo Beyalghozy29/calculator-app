a51df8010db165be612e5425134f780ab5184711
beyalghozy29 <muhammad.2903@students.amikom.ac.id>
2020-03-09 17:00:39 +0700

    Calculator

--- Calculator.cs
+++ Calculator.cs
@@ -6,5 +6,18 @@ class Calculator
 		int b = 6;
 		
 	Console.WriteLine("Hasil penambahan {0} + {1} = {2}", a, b, penambahan(a, b));
+	Console.WriteLine("Hasil pengurangan {0} - {1} = {2}", a, b, pengurangan(a, b));
+	
+	Console.WriteLine("\n Tekan sembarang key yang keluar");
+	Console.ReadKey();
+	}
+	
+	static in penambahan (int a, int b)
+	{
+		return a + b;
+	}
+	static in pengurangan (int a, int b)
+	{
+		return a - b;
 	}
 }
